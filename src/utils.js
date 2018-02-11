05cc49bda0ae4492fc6ddcb4a7cb09e92a11c79f
Jim O'Brien <jim@jimmed.net>
2018-02-11 11:47:36 +0000

    Add missing file

---
+++ src/utils.js
@@ -0,0 +1,35 @@
+import pathToRegExp from 'path-to-regexp'
+
+export const makeLocationMatcher = (path, exact) => {
+  const pathKeys = []
+  const pathRegExp = pathToRegExp(path, pathKeys, { end: exact })
+  return (location = {}) => {
+    if (!location || !location.pathname) {
+      return null
+    }
+    const pathMatch = pathRegExp.exec(location.pathname)
+    return pathMatch && transformPathMatch(pathMatch, location, pathKeys)
+  }
+}
+
+export const transformPathMatch = ([path, ...match], location, keys) => {
+  const pathParams = keys.reduce((acc, key, index) => {
+    acc[key.name] = match[index]
+    return acc
+  }, {})
+
+  const queryParams = location.search.startsWith('?')
+    ? parseQueryString(location.search.slice(1))
+    : {}
+
+  const params = { ...pathParams, ...queryParams }
+
+  return { path, params }
+}
+
+export const parseQueryString = query =>
+  query.split('&').reduce((acc, pair) => {
+    const [key, ...value] = pair.split('=')
+    acc[key] = value.join('=')
+    return acc
+  }, {})
